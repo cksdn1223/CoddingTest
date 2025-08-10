f82e80ba68a8c5ec9d4824c56723953e143389c5
cksdn1223 <rlacksdn1223@naver.com>
2025-08-10 12:59:31 +0900

    first commit

---
+++ src/main/java/ct/week01/Ex01.java
@@ -0,0 +1,9 @@
+package ct.week01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int a = 0, A = 0, aA = 0, Aa = 0, _a = 0, $a = 0;
+//        int a A = 0, 1a = 0, if = 0, new = 0, for = 0, while = 0;
+
+    }
+}
---
+++ src/main/java/ct/week01/Ex02.java
@@ -0,0 +1,21 @@
+package ct.week01;
+/*
+자료형
+기본 int float
+long 은 뒤에 L
+float은 뒤에 F
+ */
+public class Ex02 {
+    public static void main(String[] args) {
+        byte b = 0; // 1byte
+        char c1 = 'A', c2 = 65; // 2byte
+        short s = 100;  // 2byte
+        int i = 65; // 4byte
+        long l = 10000000000L;   // 8byte
+
+        float f = 3.141592f; // 4byte
+        double d = 3.141592;    // 8byte
+
+        boolean t = true;   // 1bit(1byte)
+    }
+}
